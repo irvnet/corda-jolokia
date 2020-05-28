b0676fea93d4d5d89b96ef77a8ed54e8ac6360a5
irvnet <irvnet@gmail.com>
2020-05-28 18:17:26 -0400

    make a hole for hawtio

--- provision/mon-nsg.tf
+++ provision/mon-nsg.tf
@@ -15,7 +15,17 @@ resource "azurerm_network_security_group" "jmontest-nsg" {
     destination_address_prefix = "*"
   }
 
-
+  security_rule {
+    name = "hawtio_inbound"
+    priority = 210
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "8080"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
 
 
    tags = {
